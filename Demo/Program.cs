fbae5a3b0d041a468429962a9c1da8b01653bf74
khaledHassan2 <khaledamalb22@gmail.com>
2025-01-27 02:40:22 +0200

    What Is LINQ

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region What Is LINQ
+            //List<int> Numbers = new List<int>(10) { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //List<int> OddNumber= Numbers.Where(n => n%2==1 ).ToList();
+            //foreach (var item in OddNumber)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
         }
     }
 }
